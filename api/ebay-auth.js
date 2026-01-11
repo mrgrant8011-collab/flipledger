fd3e2a81a8912609ac13b380f99dc830b95f9d59
mrgrant8011-collab <mrgrant8011@gmail.com>
2026-01-11 05:20:14 -0700

    Add files via upload

--- api/ebay-auth.js
+++ api/ebay-auth.js
@@ -3,6 +3,24 @@ export default function handler(req, res) {
   const clientId = process.env.EBAY_CLIENT_ID;
   const ruName = process.env.EBAY_RU_NAME;
   
+  // Debug mode - show what we have
+  if (req.query.debug === 'true') {
+    return res.json({
+      hasClientId: !!clientId,
+      clientIdPreview: clientId ? clientId.substring(0, 20) + '...' : 'NOT SET',
+      hasRuName: !!ruName,
+      ruNamePreview: ruName ? ruName.substring(0, 20) + '...' : 'NOT SET'
+    });
+  }
+  
+  if (!clientId || !ruName) {
+    return res.status(500).json({ 
+      error: 'Missing environment variables',
+      hasClientId: !!clientId,
+      hasRuName: !!ruName
+    });
+  }
+  
   // OAuth scopes we need
   const scopes = [
     'https://api.ebay.com/oauth/api_scope',
