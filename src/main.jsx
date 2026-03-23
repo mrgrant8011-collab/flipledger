5e6540c12d856c17d804885b8e42b3f582e658ba
mrgrant8011-collab <mrgrant8011@gmail.com>
2026-03-23 05:01:52 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import Privacy from './Privacy.jsx'
+import Terms from './Terms.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
