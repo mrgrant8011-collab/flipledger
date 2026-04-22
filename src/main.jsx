190ed9a55956d7d647e37042cc2d93e2e6814868
mrgrant8011-collab <mrgrant8011@gmail.com>
2026-04-22 09:38:04 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import Privacy from './Privacy.jsx'
 import Terms from './Terms.jsx'
+import ResetPassword from './ResetPassword.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/terms" element={<Terms />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="*" element={<App />} />
       </Routes>
     </BrowserRouter>
